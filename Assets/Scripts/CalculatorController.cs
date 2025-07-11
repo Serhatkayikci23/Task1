42457fb2d560123a86f55dd61855c59ad4979575
Serhatkayikci23 <Serhatkayikci23@gmail.com>
2025-07-11 13:40:02 +0300

    -butonlar tamamlandı
    -tüm butonlara clicked özelliği atandı

--- Assets/Scripts/CalculatorController.cs
+++ Assets/Scripts/CalculatorController.cs
@@ -1,49 +1,57 @@
 using UnityEngine;
+using TMPro;
+
 
 public class CalculatorController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+    }
+
+   
+            public void GetOperation(string operation)
+            {
+                Debug.Log(operation + " clicked");
+
+
+
+            }
+
+            public void GetNumber(int number)
+            {
+                Debug.Log(number + " clicked");
+
+
+
+
+             }
+
+            public void GetDelete(string delete)
+            {
+                Debug.Log(delete + " clicked");
+
+            }
+
+            public void GetDeleteLast(string last)
+            {
+                Debug.Log(last + " clicked");
+
+            }
+
+            public void GetComma( string comma)
+            {
+                Debug.Log(comma + " clicked");
+
+            }
+
+            public void GetResult (string result)
+            {
+                Debug.Log(result + " clicked");
+
+            }
+
         
-    }
-
-    public void GetOperation(string operation)
-    {
-        Debug.Log(operation + " clicked");
-
-
-    }
-
-    public void GetNumber(int number)
-    {
-        Debug.Log(number + " clicked");
-        
-
-
-
-    }
-
-    public void GetDelete(string delete)
-    {
-        Debug.Log(delete + " clicked");
-    }
-
-    public void GetDeleteLast(string last)
-    {
-        Debug.Log(last + " clicked");        
-    }
-
-    public void GetComma( string comma)
-    {
-        Debug.Log(comma + " clicked");
-    }
-
-    public void GetResult (string result)
-    {
-        Debug.Log(result + " clicked");
-    }
-
-
 
 }
