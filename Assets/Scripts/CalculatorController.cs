e22692719f6ff3a1ab5ca219b25a3dde810261c6
Serhatkayikci23 <Serhatkayikci23@gmail.com>
2025-07-11 11:50:30 +0300

    -tuşlara atama yapıldı
    -tuşlara komut ataması yapıldı basınca clicked özelliği

--- Assets/Scripts/CalculatorController.cs
+++ Assets/Scripts/CalculatorController.cs
@@ -8,13 +8,42 @@ public class CalculatorController : MonoBehaviour
         
     }
 
-    public void GetNumber(int number)
+    public void GetOperation(string operation)
     {
-        Debug.Log(number + " clicked");
+        Debug.Log(operation + " clicked");
 
-       
 
     }
 
-    
+    public void GetNumber(int number)
+    {
+        Debug.Log(number + " clicked");
+        
+
+
+
+    }
+
+    public void GetDelete(string delete)
+    {
+        Debug.Log(delete + " clicked");
+    }
+
+    public void GetDeleteLast(string last)
+    {
+        Debug.Log(last + " clicked");        
+    }
+
+    public void GetComma( string comma)
+    {
+        Debug.Log(comma + " clicked");
+    }
+
+    public void GetResult (string result)
+    {
+        Debug.Log(result + " clicked");
+    }
+
+
+
 }
