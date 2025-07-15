7fba481272b457f5595d578ddef347a123e4d030
Serhatkayikci23 <Serhatkayikci23@gmail.com>
2025-07-15 06:03:23 +0300

    -buton güncellemesi, işlem yapma,
    -buton güncellemesi yapıldı
    -işlemler yapılacak duruma geldi
    -ondalık ayırıcı eklendi

--- Assets/Scripts/CalculatorController.cs
+++ Assets/Scripts/CalculatorController.cs
@@ -1,30 +1,143 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 
 
 public class CalculatorController : MonoBehaviour
 {
+     public TextMeshProUGUI resultText;
+     private string currentIn = "";
+    private double result = 0.0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+         updateDisplay();
     }
 
+    public void OnButtonClick(string buttonValue)
+    {
+        if (buttonValue == "=")
+        {
+            calculateResult();
+        }
+        else if (buttonValue == "C")
+        {
+            clearInp();
+        }
+        else
+        {
+            currentIn += buttonValue;
+            updateDisplay();
+        }
+    }
+
+    
+    public void GetNumber(int number)
+    {
+        currentIn += number.ToString();
+        updateDisplay();
+    }
+
+    
+    public void GetOperation(string operation)
+    {
+        currentIn += operation;
+        updateDisplay();
+    }
+
+    
+    public void GetDelete(string delete)
+    {
+        clearInp();
+    }
+
+    
+    public void GetDeleteLast(string last)
+    {
+        if (currentIn.Length > 0)
+        {
+            currentIn = currentIn.Substring(0, currentIn.Length - 1);
+            updateDisplay();
+        }
+    }
+
+    
+    public void GetComma(string comma)
+    {
+        currentIn += ".";
+        updateDisplay();
+    }
+
+    
+    public void GetResult(string result)
+    {
+        calculateResult();
+    }
+
+    
+    public void ToggleNegative()
+    {
+        if (currentIn.StartsWith("-"))
+            currentIn = currentIn.Substring(1);
+        else
+            currentIn = "-" + currentIn;
+
+        updateDisplay();
+    }
+
+    
+    private void calculateResult()
+    {
+        try
+        {
+            result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentIn, ""));
+            currentIn = result.ToString();
+            updateDisplay();
+        }
+        catch (System.Exception)
+        {
+            currentIn = "Error!!";
+            updateDisplay();
+        }
+    }
+
+    
+    private void updateDisplay()
+    {
+        resultText.text = currentIn;
+    }
+
+    
+    private void clearInp()
+    {
+        currentIn = "";
+        result = 0.0;
+        updateDisplay();
+    }
+
+
+
    
-            public void GetOperation(string operation)
-            {
-                Debug.Log(operation + " clicked");
+
+/*
 
 
 
-            }
+    public void GetOperation(string operation)
+    {
+        Debug.Log(operation + " clicked");
+
+
+
+
+    }
 
             public void GetNumber(int number)
             {
                 Debug.Log(number + " clicked");
 
-
-
+        
 
              }
 
@@ -52,6 +165,6 @@ public class CalculatorController : MonoBehaviour
 
             }
 
-        
+        */
 
 }
